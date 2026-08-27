6761d8f6e530ba3bf1f9740d17db169cd72fae2f
J-mar19 <balendezjacob123@gmail.com>
2026-08-27 08:39:38 +0800

    Added borrow and return

--- backups/lab3.sql
+++ backups/lab3.sql
@@ -18,10 +18,34 @@ create table books(
 insert into borrow(student_id, book_id) values (1,1),(1,2),(3,2);
 
 select br.borrow_id, s.student_id,
-    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, s.student_course,
-    b.book_title, b.book_author, b.book_category,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    b.book_title, b.book_author, b.boo_category,
     br.borrow_date 
 FROM borrow br
     JOIN students s ON br.student_id = s.student_id
-    JOIN books b ON br.book_id = s.student_id
+    JOIN books b ON br.book_id = b.book_id
+ORDER BY br.borrow_date DESC; 
+
+ALTER TABLE borrow 
+MODIFY borrow_return_date TIMESTAMP NULL DEFAULT NULL;
+
+UPDATE borrow
+SET borrow_return_date = NULL
+WHERE borrow_return_date = '2026-08-25 23:24:17';
+
+--RETURN A BOOK BY UPDATTNG THE BORROW_RETURN_DATE TO THE CURRENT TIMESTAMP
+
+UPDATE borrow
+SET borrow_return_date = CURRENT_TIMESTAMP
+--change value of borrow_id to the ID of the borrow record you want to update
+WHERE borrow_id = 1 AND borrow_return_date IS NULL;
+
+select br.borrow_id, s.student_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    b.book_title, b.book_author, b.boo_category,
+    br.borrow_date, br.borrow_return_date
+FROM borrow br
+    JOIN students s ON br.student_id = s.student_id
+    JOIN books b ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NOT NULL
 ORDER BY br.borrow_date DESC; 
--- readme.txt
+++ readme.txt
@@ -23,12 +23,12 @@ INSERT METHOD -- INSERT INTO students(student_first_name,student_last_name,stude
 # utility commands
 \! cls 
 
-mysqldump -u root -p  --databases library_db > D:\dev\it30b\backups
+mysqldump -u root -p  --databases library_db > C:\it30b\backups
 
-retrieve database/backup time -- source D:\dev\it30b\backups\08182026_library_db.sql (directory of the backup)
+retrieve database/backup time -- source C:\it30b\backups\08182026_library_db.sql (directory of the backup)
 
 
-automated with date and time backup -- mysqldump -u root -p --databases library_db > "D:\dev\it30b\backups\%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%_library_db.sql
+automated with date and time backup -- mysqldump -u root -p --databases library_db > "C:\it30b\backups\%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%_library_db.sql
 
 lab 2
 
